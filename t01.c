8c2f865ede0f3b1b3452cbcbd652942bbb49bbac
LucasCP <ice0life@gmail.com>
2015-08-03 20:44:47 -0300

    ponto1

--- t01.c
+++ t01.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+/**
+ *	Trabalho 1  cifra de cesar + transposição + vigenere + substituição
+ * 
+ * 
+ * 
+ * 
+ **/
+
+int main(int args,char args[][]){
+	FILE *arq=NULL;
+	char c;
+	int key=13;
+	if((arq=fopen(args[1],"r+b"))==NULL){
+		printf("nao foi possivel abrir ( - _ - )");
+		exit(0);
+	}
+	fread(&c,sizeof(char),1,arq);
+	fseek(arq,-1,SEEK_CUR);
+	c=(c+key)%256;
+	fwrite(&c,sizeof(char),1,arq);
+	
 	return 0;
 }
