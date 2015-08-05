f1de6d88d3fd92655569625be379c52db4c881d9
Lucas C. P. <lucacp@users.noreply.github.com>
2015-08-04 21:41:51 -0300

    update 01 cesar

--- t01.c
+++ t01.c
@@ -9,18 +9,36 @@
  * 
  **/
 
-int main(int args,char args[][]){
+int main(int argc,char* args[]){
 	FILE *arq=NULL;
-	char c;
-	int key=13;
+	char c='0';
+	int key=3,i=0,k=0;
 	if((arq=fopen(args[1],"r+b"))==NULL){
-		printf("nao foi possivel abrir ( - _ - )");
+		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
 	}
-	fread(&c,sizeof(char),1,arq);
-	fseek(arq,-1,SEEK_CUR);
-	c=(c+key)%256;
-	fwrite(&c,sizeof(char),1,arq);
-	
+	//printf("%s\n",args[0]);
+	//printf("%s\n",args[1]);
+	fseek(arq,0,SEEK_END);
+	i=ftell(arq);	
+	while(i<=k){
+		fread(&c,sizeof(char),1,arq);
+		k++;
+		fseek(arq,-1,SEEK_CUR);
+		printf("%c",c);
+		c=(c+key);
+		fwrite(&c,sizeof(char),1,arq);
+		if(feof(arq)) break;
+	};
+	printf(" (-_-)\n");
+	rewind(arq);
+	k=0;
+	while(i<=k){
+		if(feof(arq)) break;
+		fread(&c,sizeof(char),1,arq);
+		printf("%c",c);
+		k++;
+	};
+	fclose(arq);
 	return 0;
 }
