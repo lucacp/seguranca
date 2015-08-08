b5eb2d1948f976bbf89173672da3342e7743433f
lucacp <ice0life@gmail.com>
2015-08-08 13:54:19 -0300

    mudanca do nome das variaveis

--- t01.c
+++ t01.c
@@ -11,7 +11,7 @@
 int main(int argc,char* args[]){
 	FILE *arq=NULL;
 	char buf[MAX]={"\0"},c;
-	int i=0,k=0,f=0,key=3;
+	int ind_arq=0,ind_atual=0,ind_auxiliar=0,key=3;
 	if((arq=fopen(args[1],"r+"))==NULL){
 		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
@@ -19,38 +19,38 @@ int main(int argc,char* args[]){
 	//printf("%s\n",args[0]);
 	//printf("%s\n",args[1]);
 	fseek(arq,0,SEEK_END);
-	i=ftell(arq);	
-	for(k=0;i>k;k++,f++){
+	ind_arq=ftell(arq);	
+	for(ind_atual=0;ind_arq>ind_atual;ind_atual++,ind_auxiliar++){
 		fread(&c,sizeof(char),1,arq);
 		if(feof(arq)) break;
 		//printf("%c",c);
-		buf[k]=c;
-		if(k+1==MAX){
-			for(f=0;f<MAX;f++){
-				buf[f]+=key;
+		buf[ind_atual]=c;
+		if(ind_atual+1==MAX){
+			for(ind_auxiliar=0;ind_auxiliar<MAX;ind_auxiliar++){
+				buf[ind_auxiliar]+=key;
 			};
 			fseek(arq,1-MAX,SEEK_CUR);
 			fwrite(buf,sizeof(char),MAX,arq);
-			f=0;
-			for(f=0;f<MAX;f++){
-				buf[f]='\0';
+			ind_auxiliar=0;
+			for(ind_auxiliar=0;ind_auxiliar<MAX;ind_auxiliar++){
+				buf[ind_auxiliar]='\0';
 			};
 			fflush(arq);
 		};
 	};
-	for(k=0;k<f;k++){
-		buf[k]+=key;
+	for(ind_atual=0;ind_atual<ind_auxiliar;ind_atual++){
+		buf[ind_atual]+=key;
 	};
-	fseek(arq,1-k,SEEK_CUR);
-	fwrite(buf,sizeof(char),k-1,arq);
+	fseek(arq,1-ind_atual,SEEK_CUR);
+	fwrite(buf,sizeof(char),ind_atual-1,arq);
 	//printf("(-_-)\n");
 	rewind(arq);
-	k=0;
-	while(i>k){
+	ind_atual=0;
+	while(ind_arq>ind_atual){
 		if(feof(arq)) break;
 		fread(&c,sizeof(char),1,arq);
 		//printf("%c",c);
-		k++;
+		ind_atual++;
 	};
 	printf("(>_<)\n");
 	fclose(arq);
