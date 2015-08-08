27f0c90b19fc1f88b2ef5f977c1d7f85f01f584d
lucacp <ice0life@gmail.com>
2015-08-08 11:09:50 -0300

    teste 02 windows
    falha

--- t01.c
+++ t01.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  *	Trabalho 1  cifra de cesar + transposição + vigenere + substituição
- * 
- * 
- * 
  * 
  **/
 
 int main(int argc,char* args[]){
 	FILE *arq=NULL;
-	char key=3,c;
-	int i=0,k=0;
-	if((arq=fopen(args[1],"r+"))==NULL){
+	char c;
+	int i=0,k=0,key=3,teste=0;
+	if((arq=fopen(args[1],"r+b"))==NULL){
 		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
 	}
@@ -25,8 +23,9 @@ int main(int argc,char* args[]){
 		fread(&c,sizeof(char),1,arq);
 		fseek(arq,k,SEEK_SET);
 		//printf("%c",c);
-		c=c+key;
-		//printf("%c",c);
+		teste=(int)c+key;
+		printf("%c",c);
+		c=(char)teste;
 		fwrite(&c,sizeof(char),1,arq);
 		k++;
 		if(feof(arq)) break;
