80d7bc983664219627de0a0292b6678ca39e3fb2
lucacp <ice0life@gmail.com>
2015-08-08 20:46:50 -0300

    Update 04 Windows
    primeiros 2 chars estão de acordo
    problema da falta do rewind resolvido

--- t01.c
+++ t01.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 30
 
 /**
  *	Trabalho 1  cifra de cesar + transposição + vigenere + substituição
@@ -10,8 +9,8 @@
 
 int main(int argc,char* args[]){
 	FILE *arq=NULL;
-	char buf[MAX]={"\0"},c;
-	int ind_arq=0,ind_atual=0,ind_auxiliar=0,key=3;
+	char c[2]=" ";
+	int ind_arq=0,ind_atual=0,key=3;
 	if((arq=fopen(args[1],"r+"))==NULL){
 		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
@@ -19,37 +18,25 @@ int main(int argc,char* args[]){
 	//printf("%s\n",args[0]);
 	//printf("%s\n",args[1]);
 	fseek(arq,0,SEEK_END);
-	ind_arq=ftell(arq);	
-	for(ind_atual=0;ind_arq>ind_atual;ind_atual++,ind_auxiliar++){
-		fread(&c,sizeof(char),1,arq);
+	ind_arq=ftell(arq);
+	ind_atual=0;
+	rewind(arq);
+	while(ind_arq>ind_atual){
 		if(feof(arq)) break;
-		//printf("%c",c);
-		buf[ind_atual]=c;
-		if(ind_atual+1==MAX){
-			for(ind_auxiliar=0;ind_auxiliar<MAX;ind_auxiliar++){
-				buf[ind_auxiliar]+=key;
-			};
-			fseek(arq,1-MAX,SEEK_CUR);
-			fwrite(buf,sizeof(char),MAX,arq);
-			ind_auxiliar=0;
-			for(ind_auxiliar=0;ind_auxiliar<MAX;ind_auxiliar++){
-				buf[ind_auxiliar]='\0';
-			};
-			fflush(arq);
-		};
+		c[0]=fgetc(arq);
+		printf("%c",c[0]);
+		c[0]=c[0]+key;
+		fseek(arq,ind_atual,SEEK_SET);
+		fputs(c[0],arq);
+		ind_atual++;
 	};
-	for(ind_atual=0;ind_atual<ind_auxiliar;ind_atual++){
-		buf[ind_atual]+=key;
-	};
-	fseek(arq,1-ind_atual,SEEK_CUR);
-	fwrite(buf,sizeof(char),ind_atual-1,arq);
 	//printf("(-_-)\n");
 	rewind(arq);
 	ind_atual=0;
 	while(ind_arq>ind_atual){
 		if(feof(arq)) break;
-		fread(&c,sizeof(char),1,arq);
-		//printf("%c",c);
+		c[0]=fgetc(arq);
+		printf("%c",c[0]);
 		ind_atual++;
 	};
 	printf("(>_<)\n");
