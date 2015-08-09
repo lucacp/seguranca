6f0aabb906ad552b2f41f99860ee7cd699d135f9
lucacp <ice0life@gmail.com>
2015-08-08 21:43:21 -0300

    Update 05 Cesar Windows XD
    Pronto

--- t01.c
+++ t01.c
@@ -11,10 +11,12 @@ int main(int argc,char* args[]){
 	FILE *arq=NULL;
 	char c[2]=" ";
 	int ind_arq=0,ind_atual=0,key=3;
-	if((arq=fopen(args[1],"r+"))==NULL){
+	if((arq=fopen(args[1],"r+b"))==NULL){
 		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
 	}
+	if(args[2]!=NULL)
+		key=atoi(args[2]);
 	//printf("%s\n",args[0]);
 	//printf("%s\n",args[1]);
 	fseek(arq,0,SEEK_END);
@@ -25,17 +27,18 @@ int main(int argc,char* args[]){
 		if(feof(arq)) break;
 		c[0]=fgetc(arq);
 		printf("%c",c[0]);
-		c[0]=c[0]+key;
+		c[0]=(c[0]+(char)key+126)%126;
 		fseek(arq,ind_atual,SEEK_SET);
-		fputs(c[0],arq);
+		fwrite(c,sizeof(char),1,arq);
 		ind_atual++;
+		fflush(arq);
 	};
 	//printf("(-_-)\n");
 	rewind(arq);
 	ind_atual=0;
 	while(ind_arq>ind_atual){
 		if(feof(arq)) break;
-		c[0]=fgetc(arq);
+		fread(c,sizeof(char)*1,1,arq);
 		printf("%c",c[0]);
 		ind_atual++;
 	};
