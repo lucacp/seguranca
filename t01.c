ac3fc0bc4f3e025afec163187b0132b2609bfa94
LucasCP <ice0life@gmail.com>
2015-08-25 22:10:30 -0300

    inicio comparacaoCifraTranposição (substituição,cesar e vigenere prontas)

--- t01.c
+++ t01.c
@@ -7,6 +7,7 @@
 #define KEY_ONE 12
 #define OKAY 13
 #define FILE_INVALID 1313
+#define CHARACTERES 256
 
 /**
  *	Trabalho 1  cifra de cesar + transposição + vigenere + substituição
@@ -16,13 +17,16 @@
  * 	obs: ./t01 <nomearquivobase> é feito Cesar no proprio arquivo com chave 3!!!
  * 	obs2: ./t01 <nomearquivobase> <numerochave> é Cesar com outra chave no mesmo arquivo!!
  *  obs3: -<numerochave> é decriptografação de cesar e transposição somente! para decriptografação de vigenere junto com v coloque um d ou seja vd 
- **/
-void cesarCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out);
-int transposicaoCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out);
-void vigenereCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcrpt);
-int substituicaoCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcrpt);
-int ComparacaoCifraCesar(FILE *arq1,int ind_arq,FILE *arq2);
-void comparacaoCifraVigenere(FILE *arq1,int ind_arq,FILE *arq2,char *chave);
+ * **/
+	void cesarCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out);
+	int transposicaoCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out);
+	void vigenereCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcrpt);
+	int substituicaoCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcrpt);
+	int ComparacaoCifraCesar(FILE *arq1,int ind_arq,FILE *arq2);
+	void comparacaoCifraVigenere(FILE *arq1,int ind_arq,FILE *arq2,char *chave);
+	int comparacaoCifraSubstituicao(FILE *arq1,int ind_arq,FILE *arq2,char *chave);
+ 
+ 
 int main(int argc,char* args[]){
 	FILE *arq=NULL,*out=NULL;
 	char tipo='c';
@@ -95,7 +99,9 @@ int main(int argc,char* args[]){
 			int chave=0;
 			char typ=args[3][1];
 			char chave_key[ind_arq];
+			char chave_subs[CHARACTERES];
 			memset(chave_key,0,sizeof(char)*ind_arq);
+			memset(chave_subs,0,sizeof(char)*CHARACTERES);
 			if(typ=='c'){
 				chave=ComparacaoCifraCesar(arq,ind_arq,out);
 				printf(" %d\n",chave);			
@@ -110,9 +116,12 @@ int main(int argc,char* args[]){
 			}
 			else if(typ=='s'){
 				int i=0;
-			//	comparacaoCifraSubstituicao(arq,ind_arq,out,chave_key);
-				for(i = 0; i< ind_arq;i++)
-					printf("%d",chave_key[i]);
+				i=comparacaoCifraSubstituicao(arq,ind_arq,out,chave_subs);
+				for(i = 0; i< CHARACTERES;i++){
+					if(i>0) printf(",");
+					printf("%d",chave_subs[i]);
+				};
+				printf("\n");
 			};
 			break;
 		};
@@ -137,7 +146,7 @@ void cesarCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out){
 		c[0]=fgetc(arq);
 		if(ind_arq<MAX_PRINT)
 			printf("%c",c[0]);
-		c[0]=(c[0]+(char)key+256)%256;
+		c[0]=(c[0]+(char)key+CHARACTERES)%CHARACTERES;
 		if(arquivo==1){
 			fwrite(c,sizeof(char),1,out);
 			fflush(out);
@@ -230,7 +239,7 @@ void vigenereCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcr
 		};
 		if(ind_arq<MAX_PRINT)
 			printf("%c",(char)chav);
-		c[0]=(char)(c[0]+(int)chav+256)%256;
+		c[0]=(char)(c[0]+(int)chav+CHARACTERES)%CHARACTERES;
 		if(arquivo==1){
 			fwrite(c,sizeof(char),1,out);
 			fflush(out);
@@ -245,8 +254,8 @@ void vigenereCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcr
 };
 int substituicaoCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out ,int dcrpt ){
 	char c[2]=" ",k[3];
-	int principal[256],ind_key=0,ind_atual=0,id=0,campo=0;
-	/*for( campo = 0, digito=0 ; campo < 256 ; campo++ ){ if(dcrpt==1){if( campo < id_key){bytes[campo]=campo;bytes[(int)key[campo]]=key[campo];}else if(bytes[campo]==' '){if(bytes[digito]==' '){bytes[campo]=campo;bytes[digito]=digito;digito++;}else{while(digito<256){++digito;if(bytes[digito]==' '){bytes[campo]=campo;bytes[digito]=digito;digito++;break;}};};}}else if( campo < id_key){bytes[campo]=key[campo];bytes[(int)key[campo]]=campo;}else if(bytes[campo]==' '){if(bytes[digito]==' '){bytes[campo]=digito;bytes[digito]=campo;digito++;}else{while(digito<256){++digito;if(bytes[digito]==' '){bytes[campo]=digito;bytes[digito]=campo;digito++;break;}};};};};if(dcrpt==1){for(campo=0;campo<256;campo++){bytes[campo]*=(-1);};};*/
+	int principal[CHARACTERES],ind_key=0,ind_atual=0,id=0,campo=0;
+	/*for( campo = 0, digito=0 ; campo < CHARACTERES ; campo++ ){ if(dcrpt==1){if( campo < id_key){bytes[campo]=campo;bytes[(int)key[campo]]=key[campo];}else if(bytes[campo]==' '){if(bytes[digito]==' '){bytes[campo]=campo;bytes[digito]=digito;digito++;}else{while(digito<CHARACTERES){++digito;if(bytes[digito]==' '){bytes[campo]=campo;bytes[digito]=digito;digito++;break;}};};}}else if( campo < id_key){bytes[campo]=key[campo];bytes[(int)key[campo]]=campo;}else if(bytes[campo]==' '){if(bytes[digito]==' '){bytes[campo]=digito;bytes[digito]=campo;digito++;}else{while(digito<CHARACTERES){++digito;if(bytes[digito]==' '){bytes[campo]=digito;bytes[digito]=campo;digito++;break;}};};};};if(dcrpt==1){for(campo=0;campo<CHARACTERES;campo++){bytes[campo]*=(-1);};};*/
 	FILE *chave_arq=NULL;
 	if((chave_arq=fopen(key,"r+b"))==NULL){
 		return FILE_INVALID;
@@ -267,7 +276,7 @@ int substituicaoCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out ,int
 		fflush(chave_arq);
 		fgetc(chave_arq);
 		id=atoi(k);
-		id=(id+256)%256;
+		id=(id+CHARACTERES)%CHARACTERES;
 		if(dcrpt==1)
 			principal[id]=ind_atual;
 		else
@@ -282,7 +291,7 @@ int substituicaoCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out ,int
 		if(feof(arq)) break;
 		c[0]=fgetc(arq);
 		//printf("%d",c[0]);
-		campo=(c[0]+256)%256;
+		campo=(c[0]+CHARACTERES)%CHARACTERES;
 		c[0]=principal[campo];
 		if(arquivo==1){
 			fwrite(c,sizeof(char),1,out);
@@ -306,7 +315,7 @@ int ComparacaoCifraCesar(FILE *arq1,int ind_arq,FILE *arq2){
 		fread(&c2,sizeof(char),1,arq2);
 		fflush(arq1);
 		fflush(arq2);
-		chave_atual=(c2-c1+256)%256;
+		chave_atual=(c2-c1+CHARACTERES)%CHARACTERES;
 		//printf("%d",chave_atual);
 		if(chave_atual==chave_ant){
 			alterado--;
@@ -329,7 +338,7 @@ void comparacaoCifraVigenere(FILE *arq1,int ind_arq,FILE *arq2,char *chave){
 		fread(&c2,sizeof(char),1,arq2);
 		fflush(arq1);
 		fflush(arq2);
-		chave_atual=(c2-c1+256)%256;
+		chave_atual=(c2-c1+CHARACTERES)%CHARACTERES;
 		if(chave_atual!=chave_ant){
 			chave[ind_key_atual++]=chave_atual;
 			chave_ant=chave_atual;
@@ -345,6 +354,56 @@ void comparacaoCifraVigenere(FILE *arq1,int ind_arq,FILE *arq2,char *chave){
 	//printf("%d",tam_key);
 	
 }
-void comparacaoCifraSubstituicao(FILE *arq1,int ind_arq,FILE *arq2,char *chave){
-	
+int comparacaoCifraSubstituicao(FILE *arq1,int ind_arq,FILE *arq2,char *chave){
+	/* indice é o caracter criptografado arq2
+	 * conteudo é o caracter decriptografado arq1
+	 * há chance de não utilizar todos os possiveis caracteres **/
+	char c1=' ',c2=' ';
+	int ind_atual=0,ind_chav=0,campo=0;
+	while(ind_atual<ind_arq){
+		fread(&c1,sizeof(char),1,arq1);
+		fread(&c2,sizeof(char),1,arq2);
+		fflush(arq1);
+		fflush(arq2);
+		campo=(c2+CHARACTERES)%CHARACTERES;
+		if(chave[campo]==0){
+			chave[campo]=(c1+CHARACTERES)%CHARACTERES;
+			ind_chav++;
+		}
+		ind_atual++;
+	};
+	return ind_chav;
+}
+int comparacaoCifraTransposicao(FILE *arq1,int ind_arq1,FILE *arq2){
+	/* Para cifra de transição é necessário achar os primeiros characteres "base" para achar a chave
+	 * assim quando for visto que a diferença atual aumenta, quer dizer que ainda não achou o character certo
+	 * com excesão da primeira vez que calcula a distancia.
+	 * o primeiro character está sempre no inicio do arquivo, o que resta descobrir é o segundo em diante
+	 * após o encontro do 3º - 4º character você encontra a chave desejada!
+	 * */
+	char base[5];
+	int ind_arq2=0,chave_min=0,difer_ant=0,difer_atual=0,ind_atual=0,base_atual=1;
+	fseek(arq2,0,SEEK_END);
+	ind_arq2=ftell(arq2);
+	rewind(arq2);
+	char volta[ind_arq2];
+	if(ind_arq1<ind_arq2)
+		chave_min=ind_arq2-ind_arq1;
+	fread(base,sizeof(char),4,arq1);
+	fflush(arq1);
+	fread(volta,sizeof(char),ind_arq2,arq2);
+	fflush(arq2);
+	while(ind_atual<ind_arq2){
+		if(volta[ind_atual]==base[base_atual]){
+			if(difer_ant==difer_atual&&base_atual>2)
+				break;
+			if(base_atual>1){
+				difer_ant=difer_atual;
+			}
+			difer_atual=ind_atual-base_atual-1;
+			base_atual++;
+		}	
+		ind_atual++;
+	};
+	return difer_atual;
 }
