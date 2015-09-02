2ec9a51e358a19cac296171ba099cb59ecb1c096
LucasCP <ice0life@gmail.com>
2015-09-01 22:20:19 -0300

    começo estruturas, funções ajustadas, dicionario no inicio

--- t03.cpp
+++ t03.cpp
@@ -1,12 +1,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
 #define MAX 10
-#define MAX_PRINT 112
-#define KEY_NULL 11
-#define KEY_ONE 12
 #define OKAY 13
-#define FILE_INVALID 1313
 #define CHARACTERES 256
 
 /**
@@ -14,24 +13,31 @@
  * objetivo: decifrar com cesar transposição e vigenere(curta) verificando chave com dicionario + heuristicas
  * 
  */
+using namespace std;
 	void cesarCrypt(char *base,int key,int ind_arq,char *teste);
-	int transposicaoCrypt(char *base,int key,int ind_arq,char *teste);
-	void vigenereCrypt(char *base,char *key,int ind_arq,char *teste,int dcrpt);
- 
+	void transposicaoCrypt(char *base,int key,int ind_arq,char *teste);
+	void vigenereCrypt(char *base,char *key,int ind_arq,char *teste);
+	int checkDict(char *teste,std::vector<std::string> dic);
+
+	
 int main(int argc,char* args[]){
-	FILE *arq=NULL,*out=NULL;
-	int ind_arq=0,ind_atual=0,key=1;
+	FILE *arq=NULL;
+	std::fstream out;
+	char tipo=0;
+	int ind_arq=0,ind_atual=0,key=1,result=0,total=0,ind_dic=0;
 	if((arq=fopen(args[1],"r+b"))==NULL){
 		printf("nao foi possivel abrir ( - _ - )\n");
 		exit(0);
 	}
 	if(args[2]!=NULL){
-		out=fopen(args[2],"r+b");
-		if(!out){
+		out.open(args[2],std::fstream::in | std::fstream::out | std::fstream::app);
+		if(!out.is_open()){
 			printf("nao e possivel abrir ( - _ - )\n");
 			exit(0);
 		};
 	};
+	if(args[3]!=NULL)
+		tipo=args[3][0];
 	fseek(arq,0,SEEK_END);
 	ind_arq=ftell(arq);
 	rewind(arq);
@@ -40,110 +46,91 @@ int main(int argc,char* args[]){
 	memset(teste,0,sizeof(char)*ind_arq);
 	fread(base,sizeof(char),ind_arq,arq);
 	fflush(arq);
-	
-	
 	fclose(arq);
-	fclose(out);
+	std::vector<std::string> dictionary;
+	out.seekg(0,out.end);
+	ind_dic=out.tellg();
+	out.seekg(0,out.beg);
+	char dicty[ind_dic],c=0;
+	while(ind_atual<ind_dic){
+		out.get(c);
+		dicty[ind_dic]=c;
+		ind_atual++;
+	};
+	switch(tipo){
+		case 'c':{
+			for(key=1;key<CHARACTERES;key++){
+				cesarCrypt(base,key,ind_arq,teste);
+				result=checkDict(teste,dictionary);
+				if(result > total){
+					printf("Cifra de Cesar\nChave:%d\n",key);
+					break;
+				}
+			};
+			break;
+		};
+		case 'v':{
+			
+			//vigenereCrypt(base,,ind_arq,teste);
+			result=checkDict(teste,dictionary);
+			break;
+		};
+		case 't':{
+			key=1;
+			while(result<total){
+				transposicaoCrypt(base,key,ind_arq,teste);
+				result=checkDict(teste,dictionary);
+				key++;
+			};
+			printf("Cifra de Transposição\nChave:%d\n",key);
+			break;
+		};
+	};
+	
+	out.close();
 	return 0;
 }
-int cesarCrypt(char *base,int key,int ind_arq,char *teste){
-	char c=0;
-	int ind_atual=0,chave_atual=0,chave_ant=0;
+void cesarCrypt(char *base,int key,int ind_arq,char *teste){
+	int ind_atual=0;
 	while(ind_arq>ind_atual){
-		c=(base[ind_atual]+key+CHARACTERES)%CHARACTERES;
-		teste[ind_atual]=c;
+		teste[ind_atual]=(base[ind_atual]+key+CHARACTERES)%CHARACTERES;
 		ind_atual++;
 	};
 };
-int transposicaoCrypt(FILE *arq,int key,int ind_arq,int arquivo,FILE *out){
-	char c[2]=" ";
-	int dcrpt=0,resto=0,tamMat,tamLin=0;
-	if(key==0){
-		return KEY_NULL;
-	}
-	else if(key<0){
-		key=key*(-1);	
-		dcrpt=1;
-	};
+void transposicaoCrypt(char *base,int key,int ind_arq,char *teste){
+	int resto=0,tamLin=0;
 	resto=ind_arq%key;
-	tamMat=ind_arq+resto;
 	tamLin=ind_arq/key;
 	if(resto!=0)
 		tamLin++;
-	//printf("%d",key);
-	char mat[tamMat];
-	//char matD[MAX*key];
-	int ind_atual=0,caso=0,col=0,colD=0,ind_mat=0;
-	while(ind_arq>ind_atual){
-		if(feof(arq)) break;
-		c[0]=fgetc(arq);
-		//if(ind_arq<MAX_PRINT)
-		//	printf("%c",c[0]);
-						
-		if(dcrpt==1){
-			caso=ind_atual%tamLin;
-			mat[key*caso+colD]=c[0];
-			//if(ind_arq<MAX_PRINT)
-			//	printf("%s",mat);
-			if(caso+1==tamLin)
-				colD++;
-		}else{
-			caso=ind_atual%key;
-			mat[caso*tamLin+col]=c[0];
-			//if(ind_arq<MAX_PRINT)
-				//printf("%s\n",mat[caso]);
-			if(caso+1==key)
-				col++;
-		};
-		fflush(arq);
+	int ind_atual=0,caso=0,col=0;
+	while(ind_arq>ind_atual){				
+		caso=ind_atual%tamLin;
+		teste[key*caso+col]=base[ind_atual];
+		if(caso+1==tamLin)
+			col++;
 		ind_atual++;
-		ind_mat++;
 	};
 	if(resto!=0){
 		char idc=0;
 		int i;
 		for( i = 0 ; i < resto ; i++ ){
-			mat[ind_mat]=idc;
+			teste[ind_atual]=idc;
 			idc++;
 		};
 	};
-	if(arquivo==1){
-		fwrite(mat,sizeof(char)*tamMat,1,out);
-		fflush(out);
-	}
-	else{
-		fseek(arq,0,SEEK_SET);
-		fwrite(mat,sizeof(char)*tamMat,1,arq);
-		fflush(arq);
-	};
-	return OKAY;
-};
-void vigenereCrypt(FILE *arq,char *key,int ind_arq,int arquivo,FILE *out,int dcrpt){
-	char c[2]=" ";
+}
+void vigenereCrypt(char *base,char *key,int ind_arq,char *teste){
 	int campo=0,id_key=strlen(key),chav=0,ind_atual=0;
 	while(ind_arq>ind_atual){
-		if(feof(arq)) break;
-		c[0]=fgetc(arq);
-		if(ind_arq<MAX_PRINT)
-			printf("%c",c[0]);
 		campo=ind_atual%id_key;
-		chav=(char)key[campo];
-		if (dcrpt==1)
-		{
-			chav=chav*-1;
-		};
-		if(ind_arq<MAX_PRINT)
-			printf("%c",(char)chav);
-		c[0]=(char)(c[0]+(int)chav+CHARACTERES)%CHARACTERES;
-		if(arquivo==1){
-			fwrite(c,sizeof(char),1,out);
-			fflush(out);
-		}
-		else{
-			fseek(arq,ind_atual,SEEK_SET);
-			fwrite(c,sizeof(char),1,arq);
-		};
-		fflush(arq);
-		ind_atual++;	
+		chav=key[campo];
+		chav=chav*-1;
+		teste[ind_atual]=(base[ind_atual]+chav+CHARACTERES)%CHARACTERES;
+		ind_atual++;
 	};
 };
+int checkDict(char *teste,std::vector<std::string> dic){
+	
+	return 0;
+}
