980b558de0055c0a50ac4a881e3f830b1bc7519c
LucasCP <ice0life@gmail.com>
2015-11-09 20:44:32 -0200

    terminado soma big integer

--- t06.cpp
+++ t06.cpp
@@ -1,88 +1,61 @@
 #include <iostream>
-#include <vector>
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 1123456
 using namespace std;
 
-void StrInt(vector<int> *r, string str){
-	int i=0;
-	for(i=0;i>(int)str.size();i++){
-		r->push_back(str[i]-'0');
-	};
-}
-vector<int> Operation(int op,vector<int> *s1,vector<int> *s2){
-	vector<int> r;
-	int i=0,j=0;
-	switch (op){
+void Operacao(int op,char *iner,char *iner2,char *outer){
+	int niner=0,niner2=0,nouter=0,nmaior=0;
+	switch(op){
 		case 1:{
-			for(i=0,j=0;(i<(int)s1->size()||j<(int)s2->size());i++,j++){
-				if(i>=(int)s1->size()){
-					r.push_back(s2->at(j));
-				}else if(j>=(int)s2->size()){
-					r.push_back(s1->at(i));
+			for(niner=0;niner<MAX;niner++){
+				if(iner[niner]==0){
+					break;
 				}
-				else{
-					r.push_back(s1->at(i)+s2->at(j));
+			}
+			nmaior=niner;
+			for(niner2=0;niner2<MAX;niner2++){
+				if(iner2[niner2]==0){
+					break;
 				}
-			};
-			for(i=0;i<(int)r.size();i++){
-				if(i+1==(int)r.size()){
-					if(r.at(i)>=10){
-						j=r.at(i)/10;
-						r.at(i)-=j*10;
-						r.push_back(j);
-					}
-				}else
-				if(r.at(i)>=10){
-					j=r.at(i)/10;
-					r.at(i)-=j*10;
-					r.at(i+1)+=j;
-				}
-			};
+			}
+			if(nmaior<niner2) nmaior=niner2;
 			
+			for(nouter=0;nouter<nmaior+1;nouter++){
+				outer[nouter]=((iner[nouter]-'0')+(iner2[nouter]-'0'))+'0';
+			};
+			for(nouter=0;nouter<nmaior+2;nouter++){
+				if(outer[nouter]>'9'){
+					if(outer[nouter+1]>'0')
+						outer[nouter+1]++;
+					else
+						outer[nouter+1]='1';
+					outer[nouter]-=10;
+				}else if(outer[nouter]<'0'){
+					outer[nouter]+='0';
+				}
+			};
 			break;
-		};
+		}
 	}
-	return r;
-} 
+}
 
 int main(int argc,char* args[]){
 	
-	vector<int> s1,s2,r;
-	int op=0,i=0;
-	char iner[112345];
+	int op=0;
+	char iner[MAX],iner2[MAX],outer[MAX];
 	memset(iner,0,sizeof(iner));
-	string str1;
-	string str2;
-	if(argc==0){
-		printf("\ndigite seu big integer: ");
-		getline(cin,str1);
-		printf("\ndigite outro big integer: ");
-		getline(cin,str2);
-		printf("\n1-soma\ndigite sua operacao: ");
-		scanf("%d",&op);
-	}else{
-		str1=string(args[1]);
-		if(argc==1){
-			printf("\ndigite outro big integer: ");
-			getline(cin,str2);
-			printf("\n1-soma\ndigite sua operacao: ");
-			scanf("%d",&op);
-		}else if(argc==2){
-			str2=string(args[2]);
-			printf("\n1-soma\ndigite sua operacao: ");
-			scanf("%d",&op);
-		}
-	}
-	StrInt(&s1,str1);
-	StrInt(&s2,str2);
-	r=Operation(op,&s1,&s2);
-	for(i=0;i>(int)r.size();i++){
-		printf("%d",r.at(i));
-	};
+	memset(iner2,0,sizeof(iner2));
+	memset(outer,0,sizeof(outer));
+	scanf("%s",iner);
+	getchar();
+	scanf("%s",iner2);
+	getchar();
+	//printf("\n1-soma\ndigite sua operacao: ");
+	scanf("%d",&op);
+	Operacao(op,iner,iner2,outer);
+	printf("%s\n",outer);
 
 
 	return 0;
